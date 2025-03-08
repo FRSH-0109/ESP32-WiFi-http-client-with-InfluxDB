38d079e197490c3d622ba22561aadd6b71981923
FRSH-0109 <kamilkosnik@gmail.com>
2025-03-08 13:17:09 +0100

    Init push - WiFi http client with PUSH

--- main/main.c
+++ main/main.c
@@ -31,13 +31,13 @@
 
 #include "cJSON.h"
 
+#include "SETTINGS_PRIVATE.h"
+
 /* The examples use WiFi configuration that you can set via project configuration menu
 
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      "www.N3net.pl-2.4G-onur0577"
-#define EXAMPLE_ESP_WIFI_PASS      "nDPUW86h"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  10
 
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_BOTH
@@ -174,95 +174,6 @@ void wifi_init_sta(void)
     }
 }
 
-/*
- * @brief Event handler registered to receive MQTT events
- *
- *  This function is called by the MQTT client event loop.
- *
- * @param handler_args user data registered to the event.
- * @param base Event base for the handler(always MQTT Base in this example).
- * @param event_id The id for the received event.
- * @param event_data The data for the event, esp_mqtt_event_handle_t.
- */
-// static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
-// {
-//     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
-//     esp_mqtt_event_handle_t event = event_data;
-//     esp_mqtt_client_handle_t client = event->client;
-//     int msg_id;
-//     switch ((esp_mqtt_event_id_t)event_id) {
-//     case MQTT_EVENT_CONNECTED:
-//         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-//         msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
-//         ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-
-//         msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-//         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-//         msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-//         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-//         break;
-//     case MQTT_EVENT_DISCONNECTED:
-//         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-//         break;
-
-//     case MQTT_EVENT_SUBSCRIBED:
-//         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-//         msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-//         ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-//         break;
-//     case MQTT_EVENT_UNSUBSCRIBED:
-//         ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-//         break;
-//     case MQTT_EVENT_PUBLISHED:
-//         ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-//         break;
-//     case MQTT_EVENT_DATA:
-//         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-//         printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-//         printf("DATA=%.*s\r\n", event->data_len, event->data);
-//         break;
-//     case MQTT_EVENT_ERROR:
-//         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-//         if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-//             // log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-//             // log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-//             // log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-//             ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-
-//         }
-//         break;
-//     default:
-//         ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-//         break;
-//     }
-// }
-
-// static void mqtt_app_start(void)
-// {   
-//     esp_mqtt_client_config_t mqtt_cfg = {
-//         .broker.address.uri = "mqtt://192.168.18.236:1883",
-//         .credentials.client_id = "client1",
-//         .credentials.username = "Ad",
-//         .credentials.authentication.password = "asd"
-//     };
-
-//     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-//     /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
-//     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
-
-//     /*Let's enqueue a few messages to the outbox to see the allocations*/
-//     // int msg_id;
-//     // msg_id = esp_mqtt_client_enqueue(client, "/topic1", "data_3", 0, 1, 0, true);
-//     // ESP_LOGI(TAG, "Enqueued msg_id=%d", msg_id);
-//     // msg_id = esp_mqtt_client_enqueue(client, "/topic2", "QoS2 message", 0, 2, 0, true);
-//     // ESP_LOGI(TAG, "Enqueued msg_id=%d", msg_id);
-
-//     /* Now we start the client and it's possible to see the memory usage for the operations in the outbox. */
-//     esp_mqtt_client_start(client);
-// }
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 esp_err_t client_event_post_handler(esp_http_client_event_handle_t evt)
 {
@@ -283,7 +194,7 @@ static void post_rest_function()
 {
     static uint8_t i = 0;
     esp_http_client_config_t config_post = {
-        .url = "http://192.168.18.236:8080/telegraf",
+        .url = TELEGRAF_BROKER_URL,
         //.url = "http://httpbin.org/post",
         .method = HTTP_METHOD_POST,
         .cert_pem = NULL,
